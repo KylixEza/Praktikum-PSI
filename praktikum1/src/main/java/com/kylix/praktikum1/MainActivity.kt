8eb77a6dd5e318ce4d93dac09eb54707b4a1e39b
ACER <loilyxtra@gmail.com>
2022-09-12 20:18:37 +0700

    initial commit and added praktikum 1

---
+++ praktikum1/src/main/java/com/kylix/praktikum1/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.kylix.praktikum1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kylix.praktikum1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    
+    private lateinit var binding: ActivityMainBinding
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
