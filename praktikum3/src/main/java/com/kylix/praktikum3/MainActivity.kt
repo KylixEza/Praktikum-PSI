13c98dcecf24a1989f1020b034d6309ecf26dc50
ACER <loilyxtra@gmail.com>
2022-10-06 18:23:24 +0700

    update: added implementation of 2 more buttons (radio button and chip)

--- praktikum3/src/main/java/com/kylix/praktikum3/MainActivity.kt
+++ praktikum3/src/main/java/com/kylix/praktikum3/MainActivity.kt
@@ -2,47 +2,11 @@ package com.kylix.praktikum3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 
 class MainActivity : AppCompatActivity() {
-    
-    companion object {
-        const val TAG = "MainActivity Lifecycle"
-    }
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d(TAG, "onCreate")
-    }
-    
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG, "onStart")
-    }
-    
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG, "onResume")
-    }
-    
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "onPause")
-    }
-    
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "onStop")
-    }
-    
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "onDestroy")
-    }
-    
-    override fun onRestart() {
-        super.onRestart()
-        Log.d(TAG, "onRestart")
     }
 }
